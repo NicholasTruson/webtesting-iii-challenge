a578ec1d633db91d9e4d33159a80f20f1152ecf1
Nicholas Truson <nicholas.truson@gmail.com>
2019-09-17 09:06:31 -0700

    mvp all 12 passing

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -2,18 +2,43 @@
 
 // IMPORTS
 
+import React from 'react'
+import { render } from '@testing-library/react'
+import 'jest-dom/extend-expect'
+import Controls from './Controls'
 
+// CONTROLS TEST
+
+describe('<Control />', () => {
 
 // BUTTONS TOGGLE CLOSED & LOCKED STATES
 
-
+it('renders btn to toggle closed and locked states', ()=>{
+    const { getByText } = render(<Controls/>)
+        getByText(/close gate/i)
+            getByText(/lock gate/i)
+  })
 
 // BUTTONS TEXT CHANGES TO REFLECT STATE
 
-
+it('renders btn text change based on state change', ()=>{
+    const { getByText } = render(<Controls/>)
+        getByText(/close gate/i)
+            getByText(/lock gate/i)
+  })
 
 // CLOSED TOGGLE IS DISABLED IF GATE IS LOCKED
 
-
+it('disables open/close btn when locked', ()=>{
+    const {getByText} = render(<Controls locked={true} />)
+        const btn = getByText(/close gate/i)
+            expect(btn).toBeDisabled()
+})
 
 // LOCKED TOGGLE BUTTON IS DISABLED IF GATE IS OPEN
+
+it('disbles lock/unlock btn when open', ()=>{
+    const { getByText } = render(<Controls closed={false} />)
+        const btn = getByText(/lock gate/i)
+            expect(btn).toBeDisabled();
+  })});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -2,6 +2,21 @@
 
 // IMPORTS
 
-
+import React from "react";
+import { render } from "react-testing-library";
+import "jest-dom/extend-expect";
+import "react-testing-library/cleanup-after-each";
+import Dashboard from "./Dashboard.js";
 
 // SHOWS THE CONTROLS AND DISPLAY
+
+describe('Dashboard test', () => {
+    it('should render controls & display',() => {
+        const { getByText } = render(<Dashboard />)
+        getByText(/unlocked/i)
+        getByText(/lock gate/i)
+        getByText(/open/i)
+        getByText(/close gate/i)
+    })
+})
+
